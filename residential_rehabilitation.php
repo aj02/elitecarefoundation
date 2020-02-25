4df7c8a0ea41a9fe0f4ca0762fbeebe0d3c2a150
Ajay Tambe <aj.tambe.02@gmail.com>
2020-02-25 13:22:20 +0530

    Added hide more option

--- residential_rehabilitation.php
+++ residential_rehabilitation.php
@@ -139,22 +139,12 @@ Managing withdrawal symptoms is vital to a successful residential program, as th
                             <p>
                             When treatment is customized to meet the specific individual’s physical, psychological, and practical needs during and after treatment, it’s more likely to help those struggling with addiction. 
 
+<p class="important margin-b25 margin-t40 grey">
 Our residential treatment facility offers evidence-based treatments and therapies offer patients a fighting chance at sobriety and recovery.
+
+</p>
     
                             </p>
-                            <ul class="list1">
-                                <li>Vivamus suscipit tortor eget felis</li>
-                                <li>Quisque velit nisi, pretium ut lacinia in</li>
-                                <li>Nonumy eirmod tempor</li>
-                            </ul>
-                            <p>
-                                <img src="images/full-post-img01.jpg" alt="" class="alignleft">
-                                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Donec rutrum congue leo eget malesuada. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus etsit dictum.
-                            </p>
-                            <p>
-                                At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
-                            </p>
-                            <p class="post-category"><a href="#" class="invertlink">Fitness</a></p>
                         </div><!-- eof blog more -->
 
                         <div class="darklinks text-center">
@@ -168,226 +158,14 @@ Our residential treatment facility offers evidence-based treatments and therapie
 
                                         <!-- Post autor -->
 
-                        <div class="author-meta with_background">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="item-media">
-                                        <img src="images/team/team1.jpg" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-md-9">
-                                    <div class="item-content">
-                                        <h4 class="entry-title">Philip Campbell</h4>
-                                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.</p>
-                                        <span class="author-social">
-                                            <a href="#" class="social-icon img-bg-icon rounded-icon soc-facebook"></a>
-                                            <a href="#" class="social-icon img-bg-icon rounded-icon soc-twitter"></a>
-                                            <a href="#" class="social-icon img-bg-icon rounded-icon soc-skype"></a>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <!-- Send comment form -->
-
-                        <div class="comment-respond" id="respond">
-                            <form class="comment-form" id="commentform" method="post" action="/">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <p class="comment-form-author">
-                                            <label for="author" class="sr-only">Name <span class="required">*</span></label>
-                                            <!-- <i class="rt-icon2-user-outline"></i> -->
-                                            <input type="text" aria-required="true" size="30" value="" name="author" id="author" class="form-control" placeholder="Full Name">
-                                        </p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <p class="comment-form-email">
-                                            <label for="comment_email" class="sr-only">Email <span class="required">*</span></label>
-                                            <!-- <i class="rt-icon2-mail2"></i> -->
-                                            <input type="email" aria-required="true" size="30" value="" name="comment_email" id="comment_email" class="form-control" placeholder="Emain Address">
-                                        </p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <p class="comment-form-site">
-                                            <label for="website" class="sr-only">Website</label>
-                                            <!-- <i class="rt-icon2-user-outline"></i> -->
-                                            <input type="text" size="30" value="" name="website" id="website" class="form-control" placeholder="Website">
-                                        </p>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <p class="comment-form-chat">
-                                            <label for="comment" class="sr-only">Comment</label>
-                                            <!-- <i class="rt-icon2-pencil3"></i> -->
-                                            <textarea aria-required="true" rows="8" cols="45" name="comment" id="comment" class="form-control" placeholder="Message..."></textarea>
-                                        </p>
-                                    </div>
-                                </div>
-                                <p class="form-submit text-center">
-                                    <button type="submit" id="submit" name="submit" class="theme_button">Send Comment</button>
-                                </p>
-                            </form>
-                        </div>
 
                         <!-- Comments -->
 
-                        <div class="comments-area" id="comments">
-                            <ol class="comment-list">
-                                <li class="comment even thread-even depth-1 parent">
-                                    <article class="comment-body media">
-                                        <div class="media-left">
-                                            <img class="media-object" alt="" src="images/team/team3_small.jpg">
-                                        </div>
-                                        <div class="media-body">
-                                    <span class="reply">
-                                        <a href="#respond" class="with_background">
-                                            <i class="rt-icon2-arrow-back-outline"></i>
-                                        </a>
-                                    </span>
-                                            <div class="comment-meta">
-                                                <a class="author_url invertlink" rel="external nofollow" href="#">Alan Smith</a>
-                                        <span class="comment-date grey">
-                                            <time datetime="2016-08-01T15:05:23+00:00" class="entry-date">22.11.2015 at 12:34</time>
-                                        </span>
-                                            </div>
-                                            <p>First Level Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                        </div>
-                                    </article>
-                                    <!-- .comment-body -->
-
-                                    <ol class="children">
-                                        <li class="comment byuser odd alt depth-2 parent">
-                                            <article class="comment-body media">
-                                                <div class="media-left">
-                                                    <img class="media-object" alt="" src="images/team/team2_small.jpg">
-                                                </div>
-                                                <div class="media-body">
-                                            <span class="reply">
-                                                <a href="#respond" class="with_background">
-                                                    <i class="rt-icon2-arrow-back-outline"></i>
-                                                </a>
-                                            </span>
-                                                    <div class="comment-meta">
-                                                        <a class="author_url invertlink" rel="external nofollow" href="#">Alan Smith</a>
-                                                <span class="comment-date">
-                                                    <time datetime="2016-08-01T15:05:23+00:00" class="entry-date grey">22.11.2015 at 12:34</time>
-                                                </span>
-                                                    </div>
-                                                    <p>Second Level Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                                </div>
-                                            </article>
-                                            <!-- .comment-body -->
-
-                                            <ol class="children">
-                                                <li class="comment byuser even depth-3">
-                                                    <article class="comment-body media">
-                                                        <div class="media-left">
-                                                            <img class="media-object" alt="" src="images/team/team10_small.jpg">
-                                                        </div>
-                                                        <div class="media-body">
-                                                    <span class="reply">
-                                                        <a href="#respond" class="with_background">
-                                                            <i class="rt-icon2-arrow-back-outline"></i>
-                                                        </a>
-                                                    </span>
-                                                            <div class="comment-meta">
-                                                                <a class="author_url invertlink" rel="external nofollow" href="#">Alan Smith</a>
-                                                        <span class="comment-date">
-                                                            <time datetime="2016-08-01T15:05:23+00:00" class="entry-date grey">22.11.2015 at 12:34</time>
-                                                        </span>
-                                                            </div>
-                                                            <p>Third Level Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                                        </div>
-                                                    </article>
-                                                    <!-- .comment-body -->
-                                                </li>
-                                                <!-- #comment-## -->
-                                            </ol>
-                                            <!-- .children -->
-                                        </li>
-                                        <!-- #comment-## -->
-                                    </ol>
-                                    <!-- .children -->
-                                </li>
-                                <!-- #comment-## -->
-
-                                <li class="comment byuser odd alt thread-odd thread-alt depth-1">
-                                    <article class="comment-body media">
-                                        <div class="media-left">
-                                            <img class="media-object" alt="" src="images/team/team4_small.jpg">
-                                        </div>
-                                        <div class="media-body">
-                                    <span class="reply">
-                                        <a href="#respond" class="with_background">
-                                            <i class="rt-icon2-arrow-back-outline"></i>
-                                        </a>
-                                    </span>
-                                            <div class="comment-meta">
-                                                <a class="author_url invertlink" rel="external nofollow" href="#">Alan Smith</a>
-                                        <span class="comment-date">
-                                            <time datetime="2016-08-01T15:05:23+00:00" class="entry-date grey">22.11.2015 at 12:34</time>
-                                        </span>
-                                            </div>
-                                            <p>First Level Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                        </div>
-                                    </article>
-                                    <!-- .comment-body -->
-                                </li>
-                                <!-- #comment-## -->
-
-                                <li class="comment byuser even thread-even depth-1">
-                                    <article class="comment-body media">
-                                        <div class="media-left">
-                                            <img class="media-object" alt="" src="images/team/team9_small.jpg">
-                                        </div>
-                                        <div class="media-body">
-                                    <span class="reply">
-                                        <a href="#respond" class="with_background">
-                                            <i class="rt-icon2-arrow-back-outline"></i>
-                                        </a>
-                                    </span>
-                                            <div class="comment-meta">
-                                                <a class="author_url invertlink" rel="external nofollow" href="#">Alan Smith</a>
-                                        <span class="comment-date">
-                                            <time datetime="2016-08-01T15:05:23+00:00" class="entry-date grey">22.11.2015 at 12:34</time>
-                                        </span>
-                                            </div>
-                                            <p>First Level Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
-                                        </div>
-                                    </article>
-                                    <!-- .comment-body -->
-                                </li>
-                                <!-- #comment-## -->
-                            </ol>
-                            <!-- .comment-list -->
-                        </div>
-
+                        
                     <!-- Navigation buttons     -->
 
-                    <div class="row nav-buttons">
-                        <div class="col-sm-6 text-center">
-                            <a href="#" class="blog-btn prev-btn">
-                                <div class="btn-image-bg">
-                                    <img src="images/blog_prev.jpg" alt="">
-                                </div>
-                                <div class="item-media">
-                                    <p>Prev</p>
-                                    <h3>Lorem ipsum dolor sit consetetur</h3>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6 text-center">
-                            <a href="#" class="blog-btn next-btn">
-                                <div class="btn-image-bg">
-                                    <img src="images/blog_next.jpg" alt="">
-                                </div>
-                                <div class="item-media">
-                                    <p>Next</p>
-                                    <h3>Sadipscing elitsed diam nonumy</h3>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                     <!-- eof Navigation buttons     -->
 
     		</div>
