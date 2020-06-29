feb47f9fd4d3d012d8398cbd0907ccb4c61690b9
aj02 <aj.tambe.02@gmail.com>
2020-06-29 19:33:28 +0530

    Update therapy.php

--- therapy.php
+++ therapy.php
@@ -276,7 +276,7 @@ include("includes/copyright.php");
 </div><!-- eof #canvas -->
 
         <script src="js/compressed.js"></script>
-        <script src="js/switcher.js"></script>
+        <!--script src="js/switcher.js"></script-->
         <script src="js/main.js"></script>
 
         <script src="js/jquery-ui.js"></script>
